cdef33f8cc6da22801b1dd1b0db4c1a05c3894c2
GakuBessho-TL <gaku.bessho@torchlight.co.jp>
2020-06-17 10:38:03 +0900

    fix the result running eslint

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,27 +1,32 @@
 <template>
   <div id="app">
-  <nav>
-    <div class="nav-wrapper">
-      <a href="#!" class="brand-logo center">Logo</a>
-      <ul class="left hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li class="active"><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
-  <p>{{ message }}</p>
+    <nav>
+      <div class="nav-wrapper">
+        <a
+          href="#!"
+          class="brand-logo center"
+        >Logo</a>
+        <ul class="left hide-on-med-and-down">
+          <li><a href="sass.html">Sass</a></li>
+          <li><a href="badges.html">Components</a></li>
+          <li class="active">
+            <a href="collapsible.html">JavaScript</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
-  export default {
-    data: function () {
-      return {
-        message: "Hello Vue!"
-      }
-    }
-  }
+export default {
+  data() {
+    return {
+      message: 'Hello Vue!',
+    };
+  },
+};
 </script>
 
 <style scoped>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,16 @@
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// require('channels');
+import '../stylesheets/application.scss';
+import 'materialize-css/dist/js/materialize';
+import Vue from 'vue';
+import App from '../app.vue';
 
-import '../stylesheets/application'
-import 'materialize-css/dist/js/materialize'
-import Vue from 'vue'
-import App from '../app.vue'
+require('@rails/ujs').start();
+require('turbolinks').start();
+require('@rails/activestorage').start();
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
-    render: h => h(App)
-  }).$mount()
-  document.body.appendChild(app.$el)
-
-  console.log(app)
-})
+    render: (h) => h(App),
+  }).$mount();
+  document.body.appendChild(app.$el);
+});
