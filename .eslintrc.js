bb622fee25ef4ccc75e7fb228d736f60c821b5a7
GakuBessho-TL <gaku.bessho@torchlight.co.jp>
2020-06-17 10:35:30 +0900

    add eslint-config-airbnb

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:vue/recommended'
+  ],
+  plugins: [
+    'node'
+  ],
+  globals: {
+    '$': true,
+    // 'google': true,
+    // 'ga': true,
+  },
+  'rules': {
+    // 開発環境ではdebuggerを許容する
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-alert': 0,
+    'prefer-destructuring': 0,
+    //importの相対パスを許可
+    'import/no-unresolved': 0,
+  },
+  'parserOptions': {
+    'sourceType': 'module'
+  }
+}
