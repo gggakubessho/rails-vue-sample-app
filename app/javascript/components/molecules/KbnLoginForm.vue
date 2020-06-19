9e960460bdabc5ce1d608c3d9741871265d28aa9
gggakubessho <funky.old.music1989@gmail.com>
2020-06-19 17:42:24 +0900

    add base vue logic to KbnLogin{Form,View}

---
+++ app/javascript/components/molecules/KbnLoginForm.vue
@@ -0,0 +1,97 @@
+<template>
+  <main>
+    <center>
+      <div class="section" />
+      <h5>Kanban App</h5>
+      <div class="section" />
+      <div class="container">
+        <div
+          class="z-depth-1 grey lighten-4 row"
+          style="display: inline-block; padding: 32px 48px 0px 48px; border: 1px solid #EEE;"
+        >
+          <form class="col s12">
+            <div class="row">
+              <div class="input-field col s12">
+                <input
+                  id="email"
+                  v-model="email"
+                  type="email"
+                  autocomplete="off"
+                  class="validate"
+                  @focus="resetError"
+                >
+                <label for="email">Enter your email</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input
+                  id="pwassword"
+                  v-model="password"
+                  type="password"
+                  autocomplete="off"
+                  class="validate"
+                  @focus="resetError"
+                >
+                <label for="password">Enter your password</label>
+              </div>
+            </div>
+            <br>
+            <center>
+              <div class="row">
+                <KbnButton
+                  :disabled="disableLoginAction"
+                  @click="handleClick"
+                >
+                  ログイン
+                </KbnButton>
+              </div>
+            </center>
+          </form>
+        </div>
+      </div>
+    </center>
+  </main>
+</template>
+
+<script>
+import KbnButton from '../atoms/KbnButton.vue'
+
+export default {
+  name: 'KbnLoginForm',
+  components: {
+    KbnButton,
+  },
+  props: {
+    onlogin: {
+      type: Function,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+      progress: false,
+      error: '',
+    }
+  },
+  computed: {
+    disableLoginAction() {
+
+    },
+  },
+  methods: {
+    resetError() {
+
+    },
+    handleClick(ev) {
+      // 子コンポーネントから受け取ったev
+    },
+  },
+
+}
+</script>
+
+<style scoped>
+</style>
--- app/javascript/components/templates/KbnLoginView.vue
+++ app/javascript/components/templates/KbnLoginView.vue
@@ -1,3 +1,25 @@
 <template>
-  <p>ログインページ</p>
+  <div class="login-view">
+    <KbnLoginForm :onlogin="handleLogin" />
+  </div>
 </template>
+
+<script>
+import KbnLoginForm from '../molecules/KbnLoginForm.vue'
+
+export default {
+  name: 'KbnLoginView',
+  components: {
+    KbnLoginForm,
+  },
+  methods: {
+    handleLogin() {
+
+    },
+    throwReject() {},
+  },
+}
+</script>
+
+<style scoped>
+</style>
