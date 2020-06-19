79743cb451f9a6383cdb87a39ff4a0257521d400
gggakubessho <funky.old.music1989@gmail.com>
2020-06-19 17:43:05 +0900

    add base vue logic to KbnBoard{Nav,View}

---
+++ app/javascript/components/molecules/KbnBoardNavigation.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <nav class="board-navigation">
+      <div class="nav-wrapper ">
+        <h1>
+          <a
+            href="#"
+            class="brand-logo"
+          >Kanban App</a>
+        </h1>
+        <ul
+          id="nav-mobile"
+          class="right hide-on-med-and-down"
+        >
+          <li>
+            <KbnButton
+              type="text"
+              @click="$emit('logout')"
+            >
+              ログアウト
+            </KbnButton>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+import KbnButton from '../atoms/KbnButton.vue'
+
+export default {
+  name: 'KbnBoardNavigation',
+
+  components: {
+    KbnButton,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
