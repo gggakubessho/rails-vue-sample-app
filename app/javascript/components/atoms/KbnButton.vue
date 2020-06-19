d44bc923431bc9d41569976f6b7f12b4ef9c332f
gggakubessho <funky.old.music1989@gmail.com>
2020-06-19 17:41:44 +0900

    add base vue logic to KbnButton

---
+++ app/javascript/components/atoms/KbnButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <button
+    class="col s12 btn btn-large waves-effect "
+    :class="classes"
+    :disabled="disabled"
+    type="button"
+    @click="handleClick"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'KbnButton',
+
+  props: {
+    type: {
+      type: String,
+      default: 'button',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    classes() {
+      const cls = this.type === 'txt' ? (`-${this.type}`) : ''
+      return [`kbn-button${cls}`]
+    },
+  },
+  methods: {
+    handleClick() {
+      this.$emit('click', ev)
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
