75a361b93997a4c24de09527cd1a1f40d8bf29dc
gggakubessho <funky.old.music1989@gmail.com>
2020-06-20 10:35:24 +0900

    fix result exec rubocop

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,13 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
+
   private
+
   def respond_with(resource, _opts = {})
-      render json: resource
+    render json: resource
   end
+
   def respond_to_on_destroy
-      head :ok
+    head :ok
   end
 end
