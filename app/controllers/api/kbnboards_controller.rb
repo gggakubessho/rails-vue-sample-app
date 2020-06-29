f5d4d155c7bf458520da2ebc5380755d9c86c88d
gggakubessho <funky.old.music1989@gmail.com>
2020-06-30 07:56:53 +0900

    fix response for tasklist

--- app/controllers/api/kbnboards_controller.rb
+++ app/controllers/api/kbnboards_controller.rb
@@ -1,7 +1,6 @@
 class Api::KbnboardsController < ApplicationController
   before_action :authenticate_user!
   def index
-    tasks = current_user.tasks.joins(:tasklist).select('tasks.*,tasklists.name as task_list_name')
-    @tasks = tasks.group_by { |task| [task[:tasklist_id], task[:task_list_name]] }
+    @tasklists = current_user.tasklists.includes(:tasks)
   end
 end
--- app/views/api/kbnboards/index.json.jbuilder
+++ app/views/api/kbnboards/index.json.jbuilder
@@ -1,1 +1,5 @@
-json.array! @tasks
+json.array!(@tasklists) do |tasklist|
+  json.id tasklist.id
+  json.name tasklist.name
+  json.items tasklist.tasks, :id, :name, :description, :tasklist_id
+end
