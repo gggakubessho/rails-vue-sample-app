29d7a3691504d5cc03ff63d4acd35cb7befc8ac8
gggakubessho <funky.old.music1989@gmail.com>
2020-08-06 08:57:58 +0900

    add CRUD action for tasks

--- app/controllers/api/kbnboards_controller.rb
+++ app/controllers/api/kbnboards_controller.rb
@@ -3,4 +3,33 @@ class Api::KbnboardsController < ApplicationController
   def index
     @tasklists = current_user.tasklists.includes(:tasks)
   end
+
+  def create
+    task = current_user.tasks.build(task_params)
+    if task.save
+      render json: { status: 'SUCCESS', data: task }
+    else
+      render json: { status: 'ERROR', data: task.errors }
+    end
+  end
+
+  def update
+    task = current_user.tasks.find(params[:id])
+    if task.update_attributes(task_params)
+      render json: { status: 'SUCCESS', message: 'Update the task', data: task }
+    else
+      render json: { status: 'ERROR', message: 'Failed to update the task', data: task.errors }
+    end
+  end
+
+  def destroy
+    task = current_user.tasks.find(params[:id]).destroy
+    render json: { status: 'SUCCESS', message: 'Delete the task', data: task }
+  end
+
+  private
+
+  def task_params
+    params.permit(:tasklist_id, :name, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   namespace :api, { format: 'json' } do
-    resources :kbnboards, only: [:index]
+    resources :kbnboards, only: [:index, :create, :update, :destroy]
   end
 end
